06b1c32418ce2b8ee0beaad2094725d0cafdebdd
Fahim Muntasir <fahimmuntasirbejoy@gmail.com>
2025-04-12 10:50:28 +0600

    some chsnge

--- fsrfrw.py
+++ fsrfrw.py
@@ -2,6 +2,14 @@ import pyautogui
 from time import sleep
 sleep(5)
 for i in range(0,4):
+    git push -u origin main
+    git push -u origin main
+    git push -u origin main
+    git push -u origin main
+    git push -u origin main
+    
+
+
 
 #   pyautogui.write('hi!how are you,,,,', interval= .5)
 #   pyautogui.press('enter')
